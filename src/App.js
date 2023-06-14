0c9b799a88189831e39f0f2f806a63ba2e865701
Arti <aartilakhlan29@gmail.com>
2023-06-14 15:27:15 +0530

    adding all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import GroupPhoto from './Components/GroupPhoto';
+import FourSection from './Components/FourSection';
+//import ImageList9 from './Components/ImageList9';
+import Photo9 from './Components/Photo9';
+import imageCompany from './Components/CompanyImage';
+import CompanyImage from './Components/CompanyImage';
+import FeedbackSection from './Components/FeedbackSection';
+import ContactSection from './Components/ContactSection';
+import TeamSection from './Components/TeamSection';
+import LocationMap from './Components/LocationMap';
+import BottomPage from './Components/BottomPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Navbar/>
+      <GroupPhoto/>
+      <FourSection/>
+      <Photo9/>
+      <CompanyImage/>
+      <FeedbackSection/>
+      <ContactSection/>
+      <TeamSection/>
+      <LocationMap/>
+      <BottomPage/>
+
+      
+     
     </div>
   );
 }
